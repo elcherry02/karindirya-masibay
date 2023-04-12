bf18102513aafc9110388c4012d67a7da12d1788
El Cherry Mae Masibay <111100461+elcherry02@users.noreply.github.com>
2023-04-12 14:08:27 +0800

    first commit

---
+++ application/controllers/Home.php
@@ -0,0 +1,28 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Home extends CI_Controller {
+
+	public function index()
+	{
+        $this->load->helper('url');
+        //set the title for the header view
+        $data['title'] = "Home";
+        $this->load->view('include/header_view', $data);
+		$this->load->view('home_view');
+		$this->load->view('include/footer_view');
+	}
+
+    public function view_products(){
+
+        $this->load->helper('url');
+        $this->load->model('Products_model');
+        //fetch all the products from the database
+        $data['products'] = $this->Products_model->getAllProducts();
+        //set the title for the header view
+        $data['title'] = "View All Products";
+        $this->load->view('include/header_view', $data);
+		$this->load->view('products_view', $data);
+		$this->load->view('include/footer_view');
+    }
+}
---
+++ application/views/home_view.php
@@ -0,0 +1,9 @@
+<div class="row">
+    <div class="col-4"></div>
+    <div class="col mt-5">
+    <h2>Welcome to our Karindirya</h2>
+<a href="<?= base_url('Home/view_products') ?>" class="btn btn-primary">View Items</a>
+
+    </div>
+</div>
+
